ee3744c0103505c15abb29d7c66fc2358037d768
jhmd <jhmd@DESKTOP-752HCCS.localdomain>
2021-01-26 20:01:54 -0800

    Starting project over

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Writing Jekyll Posts",
-        "excerpt":"My First Post  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/writing-jekyll-posts/",
-        "teaser":null}]
--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: default
-title: About
-permalink: /about/
----
-
-This is the about page. It is under construction.
